3ad8956c7f4368ca5fd021bae6869302a6ed5947
jasimahmedkhan <jasimahmedkhan@gmail.com>
2021-09-15 14:03:12 +0200

    Adding home and about pages

--- flaskblog.py
+++ flaskblog.py
@@ -1,15 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
+
+posts = [
+    {
+        "author": "Jasim Ahmed",
+        "title": "Blog post 1",
+        "content": "second post content",
+        "date_posted": "April 21, 2021"
+    },    
+    {
+        "author": "Jane Doe",
+        "title": "Blog post 2",
+        "content": "first post content",
+        "date_posted": "June 28, 2020"
+    }
+]
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return "<h1>Home Page!<h1>"
+    return render_template("home.html", posts=posts)
+    # return "<h1>Home Page!<h1>"
 
 @app.route("/about")
 def about():
-    return "<h1> About page is running<h1>"
+    return render_template("about.html", title="About")
+    # return "<h1> About page is running<h1>"
 
 if __name__ == "__main__":
     app.run(debug=True)
